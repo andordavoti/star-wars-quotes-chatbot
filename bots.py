6588caa8c92a817b6f87ab47d0305ed1cdf192bb
Andor Davoti <andor.davoti@gmail.com>
2022-03-10 15:44:54 +0100

    implement bots

---
+++ bots.py
@@ -0,0 +1,40 @@
+bots = {
+    'anakin': [
+        'I am your father',
+        'Are You Allowed To Love? I Thought That Was Forbidden For A Jedi.'
+        'You\'re asking me to be rational. I know that is something I cannot do.',
+        'I\'ve got a bad feeling about this.',
+        'You underestimate my power!',
+    ],
+    'padme': [
+        'To Be Angry Is To Be Human.',
+        'Sometimes There Are Things That No One Can Fix.',
+        'All Mentors Have A Way Of Seeing More Of Our Faults Than We Would Like. It\'s The Only Way We Grow.',
+        '...I Call It Aggressive Negotiations.',
+        'What If The Democracy We Thought We Were Serving No Longer Exists?',
+        'You\'re Going Down A Path I Can\'t Follow.'
+    ],
+    'leia': [
+        'I don\'t know where you get your delusions, Laser-Brain.',
+        'It\'s a wonder you\'re still alive.',
+        'Would it help if I got out and pushed?',
+        'You have your moments. Not many of them, but you do have them.',
+        'When did that ever help? And don’t say the Death Star.'
+    ],
+    'luke': [
+        'The Force is not a power you have. It’s not about lifting rocks. It’s the energy between all things, a tension, a balance, that binds the universe together.',
+        'I want to learn the ways of the Force and become a Jedi like my father.',
+        'The force Is strong in my family.',
+        'I won\'t fail you. I\'m not afraid.',
+        'Your thoughts betray you, Father. I feel the good in you, the conflict.'
+        'Strike me down in anger and I\'ll always be with you, just like your father.'
+    ],
+    'rey': [
+        'I Know All About Waiting.',
+        'Stop Taking My Hand!',
+        'I Didn\'t Know There Was This Much Green In The Whole Galaxy.',
+        'You Will Remove These Restraints And Leave This Cell With The Door Open.',
+        'I\'ve Seen Your Daily Routine. You Are Not Busy!',
+        'Do You Have Something — A Towel, Or Something — You Can Put On?'
+    ],
+}
--- server.py
+++ server.py
@@ -1,8 +1,47 @@
-# import socket library
 from socket import *
-
-# import sys library
+import random
 import sys
+from bots import *
+
+
+def analyzeMessage(message: str, c: socket):
+    words = message.lower().replace('?', '').split()
+
+    responded = False
+
+    for word in words:
+        if(word in bots.keys()):
+            messageToClient = word + ': ' + random.choice(bots[word])
+
+            # send message to client, encode it with UTF-8
+            c.send(bytes(messageToClient, 'utf-8'))
+
+            print(messageToClient)
+            responded = True
+            break
+
+    # if the client didn't include a bot name in the message, send a quote from a random bot
+    if not responded:
+        randomBot = random.choice(list(bots.keys()))
+
+        messageToClient = randomBot + ': ' + random.choice(bots[randomBot])
+
+        c.send(bytes(messageToClient, 'utf-8'))
+
+
+def handleMessage(message: str, c: socket):
+    if(message == 'exit'):
+        print('\nclient has left the chat room')
+
+        # send goodbye message to client, encode it with UTF-8
+        c.send(bytes('\ngoodbye!', 'utf-8'))
+
+        # close client socket
+        c.close()
+        sys.exit()
+
+    analyzeMessage(message, c)
+
 
 try:
     # create a server socket using TCP/IP protocol
@@ -13,54 +52,43 @@ try:
     port = int(sys.argv[2])
 
     # cli help option
-    if(ip == '--help' or ip == '- h'):
+    if(ip == '--help' or ip == '-h'):
         print('usage: python3 server.py <ip> <port>')
         print('example: python3 server.py localhost 9999')
         sys.exit()
 
-    # bind the socketwith host and port number
+    # bind the socket with host and port number
     s.bind((ip, port))
 
     # allow maximum 1 connection to the socket
     s.listen(1)
-
-    print('server is running...')
+    print('server is running!')
+    print('waiting for a client to connect...')
 
     # wait till a client accepts the connection
     c, addr = s.accept()
 
     # display client address
-    print("connection from: ", str(addr))
+    print("connected client: ", str(addr))
 
     try:
         # loop to receive messages from client
         while True:
             # read message from client, decode it in UTF-8
             message = c.recv(2048).decode('utf-8')
+            handleMessage(message, c)
 
-            if(message == 'exit'):
-                print('\nclient has left the chat room')
-
-                # send goodbye message to client, encode it with UTF-8
-                c.send(bytes('\ngoodbye!', 'utf-8'))
-
-                # close client socket
-                c.close()
-                break
-
-            # display message from client
-            print("Message from client: ", message)
-
-            # send modified message to client, encode it with UTF-8
-            c.send(bytes('Your message was: ' + message, 'utf-8'))
-
+    # handle if the client disconnects
     except BrokenPipeError:
         print("\nclient has left the chat room!")
     except ConnectionResetError:
         print("\nclient has left the chat room!")
 
+# handle wrong cli args
 except (IndexError, ValueError):
     print("usage: python3 server.py <ip> <port>")
     print('example: python3 server.py localhost 9999')
+
+# handle if the server disconnects
 except KeyboardInterrupt:
     print("\ngoodbye!")
